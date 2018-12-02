84e0e1cf5ed35a79c6253645cd5c8e1971d26dd4
Szabolcs Kurdi <szabolcs.kurdi@gmail.com>
2018-12-02 23:38:56 +0100

    refactor

--- src/common.js
+++ src/common.js
@@ -5,21 +5,23 @@ window.szk = (() => {
 
   ret.$$ = selector => Array.from(document.getElementsByClassName(selector))
 
-  ret.putPixel = (ctx, x, y, r = 0, g = 0, b = 0, a = 1) => {
+  const putPixel = (ctx, x, y, r = 0, g = 0, b = 0, a = 1) => {
     ctx.fillStyle = `rgb(${r},${g},${b})`
     ctx.fillRect(x, y, 1, 1)
   }
 
   // create a canvas and append it to an element
-  ret.addCanvas = (targetId, size) => {
+  const addCanvas = (targetId, size) => {
     const canvasEl = document.createElement('canvas')
     Object.assign(canvasEl, {
       width: size,
       height: size,
       id: 'canvas'
     })
-    $(targetId).innerHTML = ''
-    $(targetId).appendChild(canvasEl)
+    if (targetId) {
+      $(targetId).innerHTML = ''
+      $(targetId).appendChild(canvasEl)
+    }
     return canvasEl
   }
 
@@ -54,7 +56,7 @@ window.szk = (() => {
   // first two colors are the size of the data
   // followed by a byte stream in the [r, g, b] format
   // (the remnants in the end are trailing zeroes)
-  ret.strToRgbStream = (s) => {
+  const strToRgbStream = (s) => {
     let bytes = strToByteStream(s)
     const size = longToByteArray(bytes.length)
     const ret = [ size.slice(0, 3), size.slice(3, 6) ] // six bytes for the total size
@@ -63,6 +65,29 @@ window.szk = (() => {
       ret.push([bytes[i] || 0, bytes[i + 1] || 0, bytes[i + 2] || 0])
     }
     return ret
+  }
+
+  // main encoder
+  ret.createEncodedCanvas = (s, targetId) => {
+    let inputText = $('inputText').value
+    const pixels = strToRgbStream(inputText)
+    const sqrSize = Math.ceil(Math.sqrt(pixels.length))
+    const ctx = addCanvas('canvasWrapper', sqrSize).getContext('2d')
+
+    // customized putPixel
+    const pp = (x, y, rgb) => putPixel(ctx, x, y, rgb[0] || 0, rgb[1] || 0, rgb[2] || 0, 1)
+
+    // fill the image
+    let row = 0
+    let col = 0
+    for (let i = 0; i < sqrSize * sqrSize; i++) {
+      if (col > sqrSize - 1) {
+        col = 0
+        row++
+      }
+      pp(col, row, pixels[i] || [])
+      col++
+    }
   }
 
   // extract these functions into the caller's namespace
