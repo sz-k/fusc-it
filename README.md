025d506f556875b9b9037bca1027431400d59b49
Szabolcs Kurdi <szabolcs.kurdi@gmail.com>
2018-12-04 23:11:34 +0100

    mosaic effect

--- README.md
+++ README.md
@@ -4,6 +4,11 @@ obfuscators for fun
 
 [sz-k.github.io/fusc-it](https://sz-k.github.io/fusc-it/)
 
+## dummy server
+
+1. npm i -g serve
+2. npm start
+
 ## vendor libs
 
 - [reimg](https://github.com/gillyb/reimg) (MIT)
--- src/common.js
+++ src/common.js
@@ -90,6 +90,62 @@ window.szk = (() => {
     }
   }
 
+  // load image onto canvas
+  ret.loadImageToCanvas = (elId) => new Promise((success, failure) => {
+    const canvas = document.createElement('canvas')
+    const image = $(elId)
+    const prep = () => {
+      canvas.width = image.width
+      canvas.height = image.height
+      const ctx = canvas.getContext('2d')
+      ctx.drawImage(image, 0, 0)
+      success(canvas)
+    }
+    if (image.complete) {
+      prep()
+    } else {
+      image.onload = prep
+    }
+  })
+
+  // get a grid of small images
+  ret.getMosaics = (canvas, width = 10, height = 10) => {
+    const ctx = canvas.getContext('2d')
+    const rows = []
+    let cells = []
+    const rowCount = Math.floor(canvas.height / height)
+    const colCount = Math.floor(canvas.width / width)
+    const bufferCanvas = document.createElement('canvas')
+    Object.assign(bufferCanvas, { width, height })
+    const bufferCtx = bufferCanvas.getContext('2d')
+
+    for (let y = 0; y < rowCount; y++) {
+      for (let x = 0; x < colCount; x++) {
+        const imageData = ctx.getImageData(x * width, y * height, (x + 1) * width, (y + 1) * height)
+        bufferCtx.clearRect(0, 0, bufferCanvas.width, bufferCanvas.height)
+        bufferCtx.putImageData(imageData, 0, 0)
+        const dataURL = bufferCanvas.toDataURL()
+        cells.push(dataURL)
+      }
+      rows.push(cells)
+      cells = []
+    }
+    return rows
+  }
+
+  // create a mosaic element with the data uri as an inline bg
+  ret.createMosaicEl = (x, y, w, h, dataURL) => {
+    const el = document.createElement('div')
+    Object.assign(el.style, {
+      background: `url(${dataURL}) center center no-repeat`,
+      backgroundSize: '100% 100%',
+      width: `${w}px`,
+      height: `${h}px`,
+      position: 'relative'
+    })
+    return el
+  }
+
   // extract these functions into the caller's namespace
   ret.extract = () => Object.keys(ret).reduce((acc, key) => acc + `var ${key}=szk['${key}'];`, '')
 
